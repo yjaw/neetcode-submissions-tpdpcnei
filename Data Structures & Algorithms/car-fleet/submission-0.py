f32626c9cc8a5b770b374f7b4e1cc52fe789a002
Ying-Fang (James) Jaw <yjaw85@gmail.com>
2026-05-06 06:27:17 -0500

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        car = [(position[i], speed[i]) for i in range(len(speed))]
+        car.sort(key = lambda x: x[0])
+        res = 1
+        cur = car[-1]
+        for i in range(len(car) - 2, -1, -1):
+            if car[i][1] <= cur[1] or (target - cur[0]) / cur[1] < (target - car[i][0]) / car[i][1]:
+                cur = car[i]
+                res += 1
+        return res
+            
