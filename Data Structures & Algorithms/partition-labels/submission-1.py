5bd58898a95f8ad1166a4e39490b03465401c4dd
Ying-Fang Jaw <yjaw85@gmail.com>
2026-05-22 01:00:00 -0500

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        hmap = defaultdict(int)
+        for i in range(len(s)):
+            hmap[s[i]] = i
+        
+        res = []
+        sub_start_at = 0
+        sub_end_at = 0
+        for i in range(len(s)):
+            c = s[i]
+            sub_end_at = max(sub_end_at, hmap[c])
+            if i == sub_end_at:
+                res.append(sub_end_at - sub_start_at + 1)
+                sub_start_at = i + 1
+        return res
+
+        # time: O(N)
+        # space: O(1)  it's at most 26 lowercase letters, it's effectively O(1). Worth noting in an interview.
