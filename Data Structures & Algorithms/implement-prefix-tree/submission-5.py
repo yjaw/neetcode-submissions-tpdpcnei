301475854e8b181734ffd85c27975334f4b3789f
Ying-Fang Jaw <yjaw85@gmail.com>
2026-06-24 22:31:48 -0500

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.py
@@ -0,0 +1,33 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['.'] = {}
+    
+    def serach_helper(self, target: str) -> dict:
+        cur = self.root
+        for c in target:
+            if c not in cur:
+                return None
+            cur = cur[c]
+        return cur
+
+    def search(self, word: str) -> bool:
+        last_char = self.serach_helper(word)
+        if not last_char or '.' not in last_char:
+            return False
+        return True
+
+    def startsWith(self, prefix: str) -> bool:
+        last_char = self.serach_helper(prefix)
+        if not last_char:
+            return False
+        return True
+        
