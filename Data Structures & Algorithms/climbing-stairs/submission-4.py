c6a57026d3da368d150ea17d0a06c52696873e50
Ying-Fang Jaw <yjaw85@gmail.com>
2026-06-29 10:33:37 -0500

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp0 = 1
+        dp1 = 1
+
+        for i in range(2, n + 1):
+            dp0, dp1 = dp1, dp0 + dp1
+            
+        return dp1
+        # O(N), O(1)
