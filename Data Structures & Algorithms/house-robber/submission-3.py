d5accb55dc228f303407ae9a9f739eaffe466039
Ying-Fang Jaw <yjaw85@gmail.com>
2026-06-29 10:37:19 -0500

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob = 0
+        not_rob = 0
+
+        for num in nums:
+            rob, not_rob = not_rob + num, max(rob, not_rob)
+        
+        return max(rob, not_rob)
+        # O(N), O(1)
