e3267c550b1c65bf6a3aa47cb1804fd3fcdd0e7e
Ying-Fang Jaw <yjaw85@gmail.com>
2026-06-29 02:26:45 -0500

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        gmap = defaultdict(list)
+        res = 0
+
+        for a, b in edges:
+            gmap[a].append(b)
+            gmap[b].append(a)
+        
+        visited = set()
+
+        for i in range(n):
+            if i not in visited:
+                res += 1
+                que = deque([i])
+                visited.add(i)
+                while que:
+                    cur = que.popleft()
+                    for next_node in gmap[cur]:
+                        if next_node not in visited:
+                            visited.add(next_node)
+                            que.append(next_node)
+
+        return res
+        # O(N), O(N)
