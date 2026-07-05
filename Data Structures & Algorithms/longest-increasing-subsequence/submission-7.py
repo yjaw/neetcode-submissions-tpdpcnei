9dcfd1173296be36bfb2c4007ed1e34acaccb8f9
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-05 02:26:15 -0500

    Add: longest-increasing-subsequence - submission-7

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-7.py
@@ -0,0 +1,14 @@
+import bisect
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [float('-inf')]
+
+        for num in nums:
+            if num > dp[-1]:
+                dp.append(num)
+            else:
+                i = bisect.bisect_left(dp, num)
+                dp[i] = num
+            print(dp)
+        return len(dp) - 1
+        # O(N log N), O(N)
