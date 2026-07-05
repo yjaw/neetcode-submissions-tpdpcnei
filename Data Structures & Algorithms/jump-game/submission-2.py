c507ceb3495f938fb30641e23ab65e55a30a0126
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-05 18:24:58 -0500

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_next = 0
+        cur = 0
+
+        while cur <= max_next:
+            max_next = max(max_next, cur + nums[cur])
+            if max_next >= len(nums) - 1:
+                return True
+            cur += 1
+        
+        return False
+        # O(N), O(1)
