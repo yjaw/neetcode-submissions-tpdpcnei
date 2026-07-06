98c0b9e3886001fc7118ca58905499a15d12d06e
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-05 19:21:48 -0500

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key = lambda x: x.start)
+        cur_end = -1
+        for interval in intervals:
+            start, end = interval.start, interval.end
+            if start < cur_end:
+                return False
+            cur_end = end
+        return True
+        # O(N log N), O(1)
