4681820f1204bde2d5917443e4ad00aa15eb2ebe
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-06 20:54:07 -0500

    Add: meeting-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-11.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        pq = []
+        res = 0
+        intervals.sort(key = lambda x: x.start)
+        for interval in intervals:
+            start, end = interval.start, interval.end
+            while pq and pq[0] <= start:
+                heapq.heappop(pq)
+            heapq.heappush(pq, end)
+            res = max(res, len(pq))
+        return res
+        # O(N log N), O(N)
