208996425c2c4abb60b1d9999dab0474fc6f0d23
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-07 03:22:06 -0500

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        expect = len(nums)
+        count = 0
+        for i in range(len(nums)):
+            expect += i
+            count += nums[i]
+        return expect - count
+        # O(N), O(1)
