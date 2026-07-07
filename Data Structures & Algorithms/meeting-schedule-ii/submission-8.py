5c893ddb07011d55e068575acdc32f2f22937841
Ying-Fang Jaw <yjaw85@gmail.com>
2026-07-06 20:39:29 -0500

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        res = 0
+        room = [0] * 1000005
+        for interval in intervals:
+            lo, hi = interval.start, interval.end
+            for i in range(lo, hi):
+                room[i] += 1
+                res = max(res, room[i])
+        return res
+        # O(N * M), O(1)
